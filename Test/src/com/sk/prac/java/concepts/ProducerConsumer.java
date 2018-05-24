7da63e9ec2b8f416b0c6de787360e972a047e92d
'sandeepkalva' <'sandeep.kalva08@gmail.com'>
2018-05-24 15:02:39 +0530

    Multi-thread resource share demo- multi thread communication - ProducerConsumer Impl.

---
+++ Test/src/com/sk/prac/java/concepts/ProducerConsumer.java
@@ -0,0 +1,69 @@
+package com.sk.prac.java.concepts;
+
+import java.util.ArrayList;
+
+
+public class ProducerConsumer {
+
+	protected static ArrayList<Integer> resource = new ArrayList<>();
+	protected static int CAP =10;
+	public static void main(String[] args) {
+		produce();
+		consumer();
+	}
+
+	private static void produce() {
+		new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				for(;;) {
+					synchronized (resource) {
+						if(resource.size() == CAP)
+							try {
+								//wait on resource as we have max elements.
+								resource.wait();
+							} catch (InterruptedException e) {
+								e.printStackTrace();
+							}
+						int dd = (int) (Math.random()*10);
+						resource.add(dd);
+						System.out.println("added: "+dd);
+
+						resource.notify();
+						//notify on resource as we finished producing element.
+					}
+				}
+			}
+		}).start();
+	}
+
+
+	private static void consumer() {
+		new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				for(;;) {
+					synchronized (resource) {
+						if(resource.size() == 0)
+							try {
+								//wait on resource if it have no elements in it
+								resource.wait();
+							} catch (InterruptedException e) {
+								e.printStackTrace();
+							}
+
+						System.out.println("Removed: "+resource.remove(0));
+						resource.notify();
+						//notify on resource as we consumed the element
+
+					}
+				}
+			}
+		}).start();
+
+	}
+}
+
+
